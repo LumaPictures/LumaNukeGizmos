7505e5dac1904d001004034f245a73fc12ce9a0d
nrusch <nrusch@github>
2012-11-04 22:07:53 -0800

    Convert documentation comments to proper docstrings. General documentation/comment cleanup and expansion.

--- init.py
+++ init.py
@@ -1,66 +1,75 @@
-# To use this file, copy it as 'init.py' to a directory on your plugin path.
-# By default, the plugin path is (taken from the nuke manual):
-#
-# Linux:
-#  /users/login name/.nuke
-#  /usr/local/Nuke6.0v6/plugins
-# Mac OS X:
-#  /Users/login name/.nuke
-#  /Applications/Nuke6.0v6/Nuke6.0v6.app/Contents/MacOS/plugins
-# Windows:
-#  In the .nuke directory, which can be found under the directory pointed to
-#  by the HOME environment variable. If this variable is not set (which is
-#  common), the .nuke directory will be under the folder specified by the
-#  USERPROFILE environment variable - which is generally of the form:
-#    drive letter:\Documents and Settings\login name\      (Windows XP)
-#        or
-#    drive letter:\Users\login name\                       (Windows Vista)
-#
-# If there is already a 'init.py' at that location, open it in your favorite
-# text editor, and add the contents of this file to the end of it.
-#
-# Once installed, this script will add to the plugin path subfolders of the
-# folder it resides in (or of directories pointed at by the NUKE_GIZMO_PATH
-# environment variable, if it is defined... or you may provide a custom
-# directory by editing the CUSTOM_GIZMO_LOCATION, below), and have gizmos within
-# those subfolders automatically available in nuke
-# 
-# If in GUI mode, menu items for these subfolders may also be automatically
-# created - see 'menu.py' for details.
+'''
+Introduction
+============
+This script recursively adds path trees rooted in one or more directories to
+the Nuke plugin path. If Nuke is running in GUI mode, it can also be used to
+automatically build menu trees for any gizmo files it locates. (Note: see the
+accompanying `menu.py` file for a predefined implementation of this.)
+
+These root directories can be resolved in several ways. In order of precedence,
+they are:
+
+    1) Using the `CUSTOM_GIZMO_LOCATION` variable below, if it is set to an
+        existing directory. Note that environment variables will be expanded.
+    2) From the `NUKE_GIZMO_PATH` environment variable, if it is set.
+    3) The location of the file in which the ``GizmoPathManager`` code resides.
+    4) The Nuke plugin path.
 
 
-# Example custom gizmo locations:
+Installation
+============
+To use the ``GizmoPathManager``, copy or move this file as-is to a directory on
+your Nuke plugin path, or merge its contents with an existing `init.py` file.
+The default directories included on the plugin path are:
 
-# Linux:
-# CUSTOM_GIZMO_LOCATION = r'/users/<login name>/.nuke/Gizmos'
+Linux::
 
-# Mac OS X:
-# CUSTOM_GIZMO_LOCATION = r'/Users/<login name>/.nuke/Gizmos'
+    /users/<LOGIN NAME>/.nuke
+    /usr/local/Nuke6.2v6/plugins
 
-# Windows:
-# CUSTOM_GIZMO_LOCATION = r'C:\Users\<login name>\.nuke\Gizmos'
+Mac OS X::
 
-# WARNING: on windows, do NOT end with a trailing slash... ie this is BAD:
-# CUSTOM_GIZMO_LOCATION = r'C:\Users\<login name>\.nuke\Gizmos\'
+    /Users/<LOGIN NAME>/.nuke
+    /Applications/Nuke6.2v6/Nuke6.2v6.app/Contents/MacOS/plugins
 
-CUSTOM_GIZMO_LOCATION = r''
+Windows:
+    In the .nuke directory, which can be found under the directory pointed to
+    by the HOME environment variable. If this variable is not set (which is
+    common), the .nuke directory will be under the folder specified by the
+    USERPROFILE environment variable, which is generally in one of the
+    following forms::
 
+    <DRIVE LETTER>:\Documents and Settings\<LOGIN NAME>\    (Windows XP)
+    <DRIVE LETTER>:\Users\<LOGIN NAME>\                     (Windows Vista/7)
+'''
 import os
 import re
 
 import nuke
 
+
+# This can be used to set the root directory for the GizmoPathManager search
+# without the need to define any environment variables. Note that if this is
+# defined and points to an existing directory, other possible search locations
+# will be ignored, unless the `__main__` entry point code is modified below.
+CUSTOM_GIZMO_LOCATION = r''
+
+
 class GizmoPathManager(object):
-    def __init__(self, exclude=r'^\.', searchPaths=None):
-        '''Used to add folders within the gizmo folder(s) to the gizmo path
-        
-        exclude: a regular expression for folders / gizmos which should NOT be
-            added; by default, excludes files / folders that begin with a '.'
-            
-        searchPaths: a list of paths to recursively search; if not given, it
-            will use the NUKE_GIZMO_PATH environment variable; if that is
-            not defined, it will use the directory in which this file resides;
-            and if it cannot detect that, it will use the pluginPath 
+    '''
+    Class used to automatically add directory trees to the Nuke plugin path,
+    and to build menu items for any gizmos found in those trees.
+    '''
+    def __init__(self, searchPaths=None, exclude=r'^\.', ):
+        '''
+        'searchPaths': An iterable of paths to recursively search. If omitted,
+        the search will first try to use the `NUKE_GIZMO_PATH` environment
+        variable. If that is also undefined, it will resolve and use the
+        directory in which this file resides. If that cannot be determined, the
+        contents of the Nuke plugin path will be used.
+
+        'exclude': A regular expression for folders and gizmo files to ignore.
+        The default pattern ignores anything beginning with `.`.
         '''
         if isinstance(exclude, basestring):
             exclude = re.compile(exclude)
@@ -76,30 +85,32 @@ class GizmoPathManager(object):
                     searchPaths = list(nuke.pluginPath())
         self.searchPaths = searchPaths
         self.reset()
-        
+
     @classmethod
     def canonical_path(cls, path):
         return os.path.normcase(os.path.normpath(os.path.realpath(os.path.abspath(path))))
-        
+
     def reset(self):
         self._crawlData = {}
-        
+
     def addGizmoPaths(self):
-        '''Recursively search searchPaths for folders to add to the nuke
-        pluginPath.
+        '''
+        Recursively search ``self.searchPaths`` for folders whose names do not
+        match the exclusion pattern ``self.exclude``, and add them to the Nuke
+        plugin path.
         '''
         self.reset()
         self._visited = set()
         for gizPath in self.searchPaths:
             self._recursiveAddGizmoPaths(gizPath, self._crawlData,
                                          foldersOnly=True)
-            
+
     def _recursiveAddGizmoPaths(self, folder, crawlData, foldersOnly=False):
         # If we're in GUI mode, also store away data in _crawlDatato to be used
         # later by addGizmoMenuItems
         if not os.path.isdir(folder):
             return
-        
+
         if nuke.GUI:
             if 'files' not in crawlData:
                 crawlData['gizmos'] = []
@@ -111,7 +122,7 @@ class GizmoPathManager(object):
         if canonical_path in self._visited:
             return
         self._visited.add(canonical_path)
-        
+
         for subItem in sorted(os.listdir(canonical_path)):
             if self.exclude and self.exclude.search(subItem):
                 continue
@@ -126,28 +137,35 @@ class GizmoPathManager(object):
                 name, ext = os.path.splitext(subItem)
                 if ext == '.gizmo':
                     crawlData['gizmos'].append(name)
-                    
-    def addGizmoMenuItems(self, toolbar=None, defaultTopMenu=None):
-        '''Recursively create menu items for gizmos found on the searchPaths.
-        
-        Only call this if you're in nuke GUI mode! (ie, from inside menu.py)
-        
-        toolbar - the toolbar to which to add the menus; defaults to 'Nodes'
-        defaultTopMenu - if you do not wish to create new 'top level' menu items,
-            then top-level directories for which there is not already a top-level
-            menu will be added to this menu instead (which must already exist)
-        '''        
+
+    def addGizmoMenuItems(self, rootMenu=None, defaultTopMenu=None):
+        '''
+        Recursively creates menu items for gizmos found on this instance's
+        search paths. This only has an effect if Nuke is in GUI mode.
+
+        'rootMenu': The root Nuke menu to which the menus and menu items should
+        be added, either as a ``nuke.Menu`` object or a string. If omitted, the
+        Nuke 'Nodes' menu will be used.
+
+        'defaultTopMenu': If you do not wish to create new menu items at the
+        top level of the target parent menu, directories for which top-level
+        menus do not already exist will be added to this menu instead. This
+        must be the name of an existing menu.
+        '''
+        if not nuke.GUI:
+            return
+
         if not self._crawlData:
             self.addGizmoPaths()
-            
-        if toolbar is None:
-            toolbar = nuke.menu("Nodes")
-        elif isinstance(toolbar, basestring):
-            toolbar = nuke.menu(toolbar)
-        self._recursiveAddGizmoMenuItems(toolbar, self._crawlData,
+
+        if rootMenu is None:
+            rootMenu = nuke.menu('Nodes')
+        elif isinstance(rootMenu, basestring):
+            rootMenu = nuke.menu(rootMenu)
+        self._recursiveAddGizmoMenuItems(rootMenu, self._crawlData,
                                          defaultSubMenu=defaultTopMenu,
                                          topLevel=True)
-    
+
     def _recursiveAddGizmoMenuItems(self, toolbar, crawlData,
                                     defaultSubMenu=None, topLevel=False):
         for name in crawlData.get('gizmos', ()):
@@ -155,7 +173,7 @@ class GizmoPathManager(object):
             if niceName.find('_v')==len(name) - 4:
                 niceName = name[:-4]
             toolbar.addCommand(niceName,"nuke.createNode('%s')" % name)
-            
+
         for folder, data in crawlData.get('dirs', {}).iteritems():
             import sys
             subMenu = toolbar.findItem(folder)
@@ -165,13 +183,14 @@ class GizmoPathManager(object):
                 else:
                     subMenu = toolbar.addMenu(folder)
             self._recursiveAddGizmoMenuItems(subMenu, data)
-                    
+
+
 if __name__ == '__main__':
+    CUSTOM_GIZMO_LOCATION = os.path.expandvars(CUSTOM_GIZMO_LOCATION.strip()).rstrip('/\\')
     if CUSTOM_GIZMO_LOCATION and os.path.isdir(CUSTOM_GIZMO_LOCATION):
         gizManager = GizmoPathManager(searchPaths=[CUSTOM_GIZMO_LOCATION])
     else:
         gizManager = GizmoPathManager()
     gizManager.addGizmoPaths()
     if not nuke.GUI:
-        # We're not gonna need it anymore, cleanup...
         del gizManager
--- menu.py
+++ menu.py
@@ -1,57 +1,53 @@
-# *****************************************************************************
-# NOTE: in order for this to work, you must ALSO install the included init.py;
-# the installation instructions for it are essentially identical...
-# *****************************************************************************
-#
-# To use this file, copy it as 'menu.py' to a directory on your plugin path.
-# By default, the plugin path is (taken from the nuke manual):
-#
-# Linux:
-#  /users/login name/.nuke
-#  /usr/local/Nuke6.0v6/plugins
-# Mac OS X:
-#  /Users/login name/.nuke
-#  /Applications/Nuke6.0v6/Nuke6.0v6.app/Contents/MacOS/plugins
-# Windows:
-#  In the .nuke directory, which can be found under the directory pointed to
-#  by the HOME environment variable. If this variable is not set (which is
-#  common), the .nuke directory will be under the folder specified by the
-#  USERPROFILE environment variable - which is generally of the form:
-#    drive letter:\Documents and Settings\login name\      (Windows XP)
-#        or
-#    drive letter:\Users\login name\                       (Windows Vista)
-#
-# If there is already a 'menu.py' at that location, open it in your favorite
-# text editor, and add the contents of this file to the end of it.
-#
-# Once installed, this script will allow you to create subfolders within the
-# same directory it resides in (or in the directory pointed at by the
-# NUKE_GIZMO_PATH environment variable, if it is defined, or you may provide
-# a custom directory by editing the CUSTOM_GIZMO_LOCATION, below), and have gizmos
-# within those subfolders automatically available in nuke from the menu (or submenu)
-# of the same name.
-#
-# Ie, if your directory structure looks like this:
-#
-# /basePluginDir
-#     menu.py
-#     /Images
-#         Rainbow.gizmo
-#     /MyCustomMenu
-#         /SubMenu
-#             MakeAwesome.gizmo
-#
-# ...then when you clicked on the 'Image' menu, there would be an additional
-# item at the end to create a 'Rainbow' node.  Additionally, there would
-# be a new top-level menu, 'MyCustomMenu', which would have a 'SubMenu', which
-# would have an entry to create a 'MakeAwesome' node.
+'''
+Notes
+=====
+This file is meant to be deployed alongside the included `init.py`, and invokes
+the automatic menu item creation on the default `GizmoPathManager` instance.
+Please see the docstring of the `init.py` for installation instructions and
+further information.
 
+Once installed, this script uses the default ``GizmoPathManager`` instance to
+automatically build hierarchical menu structures and commands for any gizmo
+files it locates by recursively searching its defined path(s). These menu
+hierarchies will be structured to match the layout of the gizmo files on disk,
+allowing for easy menu reorganization without any code changes.
+
+
+Sample Folder Structure
+-----------------------
+$HOME
+    .nuke
+        Gizmos    <- $NUKE_GIZMO_PATH environment variable points here.
+            Channel    <- Each directory will generate a submenu
+                BumpNormals.gizmo    <- .gizmo files generate menu commands
+                Colormatte.gizmo
+                FacingRatio_v02.gizmo    <- '_v02' will be stripped
+            Color
+                ColorAnalyses.gizmo
+            Draw
+                AdvancedGrain.gizmo
+            Transform
+                ITransform.gizmo
+            ...
+            ...
+
+This will produce a menu structure with menu commands generated from the gizmo
+file names minus the extension and, if present, a version suffix matching the
+form '_vxx'. So in the above example tree, the file 'FacingRatio_v02.gizmo'
+will generate a menu command simply labeled 'FacingRatio'.
+
+If directory names found in the search tree match any of Nuke's built-in menu
+names (e.g. Color, Filter, Draw, etc.), the gizmo menu commands will be added
+to them. Please note that currently, if the menu command matches the name of a
+command that already exists, it will replace the existing item.
+
+
+*Example gizmo names taken from Nukepedia.com
+'''
 if __name__ == '__main__':
-  # Just in case they didn't use the supplied init.py
-  gizManager = globals().get('gizManager', None)
-  if gizManager is None:
-      print 'Problem finding GizmoPathManager - check that init.py was setup correctly'
-  else:
-      gizManager.addGizmoMenuItems()
-      # Don't need it no more...
-      del gizManager
+    gizManager = globals().get('gizManager', None)
+    if gizManager is None:
+        print 'Problem finding GizmoPathManager - check that init.py was setup correctly'
+    else:
+        gizManager.addGizmoMenuItems()
+        del gizManager
